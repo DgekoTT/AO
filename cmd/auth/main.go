4fbfaa51b140b572bb19ba7cf51f34b66ca5bfab
Dgeko <dfhdfh13@gmail.com>
2024-01-18 21:17:37 +0300

    слог натроен

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"yourTeamAuth/internal/config"
 )
 
@@ -14,5 +15,24 @@ const (
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting application", slog.String("env", cfg.Env))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
