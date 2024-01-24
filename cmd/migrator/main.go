8988cc4436bc963da50cd5f5b2ef7864088611b8
Dgeko <dfhdfh13@gmail.com>
2024-01-25 00:03:49 +0300

    написал обертку для мигратора

---
+++ cmd/migrator/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	var storagePath, migrationsPath, migrationsTable, down string
+
+	flag.StringVar(&storagePath, "storage-path", "", "path to storage")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "name of migrations table")
+	flag.StringVar(&down, "down", "", "migration down")
+	flag.Parse()
+
+	if storagePath == "" {
+		panic("storage-path is required")
+	}
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+	databaseUrl := "postgres://postgres:destro@localhost:5433/userAu?sslmode=disable"
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		databaseUrl,
+	)
+	if err != nil {
+		panic(err)
+	}
+	if down != "" {
+		if err := m.Steps(-1); err != nil {
+			if errors.Is(err, migrate.ErrNoChange) {
+				fmt.Println("no migrations to down")
+				return
+			}
+			panic(err)
+		}
+		fmt.Println("migration down")
+		return
+	}
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied")
+}
